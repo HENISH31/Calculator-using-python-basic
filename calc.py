bf2f17a0db78abf2f1839ec49cb2aacb94756693
Rana Henish <henish3120@hotmail.com>
2026-01-09 16:31:11 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,7 @@
+
+number1 = float(input("Enter a number: "))
+number2 = float(input("Enter another number: "))
+print("The value of", number1, "+", number2, "is", number1 + number2)
+print("The value of", number1, "-", number2, "is", number1 - number2)
+print("The value of", number1, "*", number2, "is", number1 * number2)
+print("The value of", number1, "/", number2, "is", number1 / number2)
